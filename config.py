d8eda3858691a88d0077858b3c8afc85af5aa66b
Ankit Parmar <ap8221710@gmail.com>
2021-10-07 12:46:29 +0530

    config removed

--- config.py
+++ config.py
@@ -2,6 +2,3 @@ username = "mymongotest"
 password = "koKTlfVkZ8ae3JeS"
 dbname = "crud_db"
 db_string = "mongodb+srv://mymongotest:koKTlfVkZ8ae3JeS@cluster0.fa0so.mongodb.net/crud_db?retryWrites=true&w=majority"
-
-
-# db_string = "mongodb+srv://{username}:{password}@cluster0.fa0so.mongodb.net/{dbname}?retryWrites=true&w=majority
