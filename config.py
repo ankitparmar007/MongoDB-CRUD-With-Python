c5dc39350dae2ac4fbe18398b4c26d68b7e58cf5
ankitparmar007 <73648141+ankitparmar007@users.noreply.github.com>
2021-10-07 12:47:45 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-username = "mymongotest"
-password = "koKTlfVkZ8ae3JeS"
-dbname = "crud_db"
-db_string = "mongodb+srv://mymongotest:koKTlfVkZ8ae3JeS@cluster0.fa0so.mongodb.net/crud_db?retryWrites=true&w=majority"
